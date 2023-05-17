e267ebc8583ccf15f3119655697b05913954100c
Jatin Dalal <jd.jatindalal@gmail.com>
2023-05-17 16:54:58 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Image captioner
+
+A simple web app that generates caption for a given image.
+
+Microsoft's GIT (GenerativeImage2Text), base-sized, fine-tuned on COCO is used here. (https://huggingface.co/microsoft/git-base-coco)
+
+During first start the app downloads the model and it is about 800 MiB in size.
+
+## Usage
+You should have PyTorch setup.  
+Note: CPU and CUDA versions of PyTorch are setup differently on different os look more at https://pytorch.org/get-started/locally/
+
+### Local
+```bash
+pip install torch streamlit transformers
+
+streamlit run app.py
+
+```
+
+Now you can reach the app at http://localhost:8501 or the port streamlit chooses if that one is busy.
+
+## Screenshot
+![screenshot](./images/screenshot.png)
+
+
+## Samples
+
+
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| ![image1](./images/Image1.png) | football player has been a key player for football team        |
+| ![image2](./images/Image2.png) | a pair of horses stand in a field with a stormy sky behind them. |
+| ![image3](./images/Image3.png) | we are all about our new logo.                                   |
---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForCausalLM
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+@st.cache_resource
+def load_captioning_model():
+    processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco").to(device)
+
+    return model, processor
+
+def generate_captions(_image, _model, _processor, num_beams=8):
+    """
+    Takes in a pillow image, the captioning model and its preprocessor
+    and returns a caption
+    """
+    pixel_values = _processor(images=_image, return_tensors="pt").to(device).pixel_values
+        
+    generated_ids = _model.generate(pixel_values=pixel_values, max_length=100, num_beams=num_beams)
+    generated_caption = _processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return generated_caption
+    
+
+model, preprocessor = load_captioning_model()
+
+st.title("Generate Image Captions")
+
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "png"])
+
+num_beams = st.slider("Set the number of beams (1: Greedy Decoding) * Increasing this increases generation time...", min_value=1, max_value=5, value=1)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image(uploaded_file)
+
+    with col3:
+        st.write(' ')
+
+    generated_caption = generate_captions(image, model, preprocessor, num_beams)
+    st.write(f"Generated Caption: \"{generated_caption}\"")
